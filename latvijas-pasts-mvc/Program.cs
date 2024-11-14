7b2d606a9ae25a6713e261227a99e4873c327188
ReinisB <balodis.reinis17@gmail.com>
2024-11-14 15:31:28 +0200

    Basic latvijas-pasts implementation.

---
+++ latvijas-pasts-mvc/Program.cs
@@ -0,0 +1,41 @@
+using latvijas_pasts_mvc.DataBase;
+using Microsoft.EntityFrameworkCore;
+
+namespace latvijas_pasts_mvc;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+        
+        builder.Services.AddDbContext<CvDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("latvijas-pasts")));
+        builder.Services.AddScoped<DbContext, CvDbContext>();
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
+}
